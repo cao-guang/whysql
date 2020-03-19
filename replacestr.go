f2977c484bc9490dbf9b7a0e491be3fd7f943814
广 <275378189@qq.com>
2020-03-19 11:14:36 +0800

    Create replacestr.go
    字符串过滤

---
+++ replacestr.go
@@ -0,0 +1,19 @@
+package whysql
+
+import "strings"
+
+func SafeReplaceParames(param string) string {
+	var a string
+	a = strings.Replace(param, "'", "‘", -1)
+	//a = strings.Replace(a, "\"", "“", -1)
+	a = strings.Replace(a, "&", "", -1)
+	a = strings.Replace(a, "#", "", -1)
+	a = strings.Replace(a, "!", "！", -1)
+	a = strings.Replace(a, "*", "", -1)
+	a = strings.Replace(a, "^", "", -1)
+	a = strings.Replace(a, "(", "（", -1)
+	a = strings.Replace(a, ")", "）", -1)
+	a = strings.Replace(a, ";", "；", -1)
+	a = strings.Replace(a, "--", "", -1)
+	return a
+}
