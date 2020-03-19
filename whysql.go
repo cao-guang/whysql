1e8a945f1234ac03b6a7f5a65549c7e0379659cb
广 <275378189@qq.com>
2020-03-19 09:37:16 +0800

    1.0

---
+++ whysql.go
@@ -0,0 +1,184 @@
+package whysql
+
+import (
+	"encoding/json"
+	"errors"
+	"strconv"
+	"strings"
+	"github.com/gohouse/gorose/v2"
+	"fmt"
+)
+
+
+func GetWhereSql_one(TabName, ParameterStr string, OrderStr string, SortStr string, PageSize, CurrentPage int) (string, error){
+	var whereSql string
+
+	return whereSql,nil
+}
+
+func GetFirstValue(arr []gorose.Data)string{
+	var value string = ""
+	if len(arr) > 0 {
+	for _, v := range arr[0] {
+	value, _ = InterfaceToString(v)
+	break
+	}
+	}
+	return value
+}
+
+func InterfaceToString(val interface{}) (string, error) {
+	var v interface{}
+	var str string
+	switch val.(type) {
+	case int64:
+		v = fmt.Sprintf("%d", val)
+		str = v.(string)
+	case string:
+		str = fmt.Sprintf("%s", val)
+	case float64:
+		t := fmt.Sprintf("%f", val)
+
+		index := strings.Index(t, ".")
+		if index > 0 {
+			str = t[:index]
+		} else {
+			str = t
+		}
+	default:
+		if b, ok := val.([]byte); ok {
+			str = string(b)
+		}
+	}
+	return str, nil
+}
+
+
+
+
+func GetWhereSqlOrderLimt(TabName, ParameterStr string, OrderStr string, SortStr string, PageSize, CurrentPage int) (string, error) {
+
+	whereSql, err := GetWhereSql(ParameterStr)
+	if err != nil {
+		return "", err
+	}
+
+	whereSql = "select * from  " + TabName + "  where " + whereSql + " Order By " + OrderStr + " " + SortStr + "  LIMIT " + strconv.Itoa((CurrentPage-1)*PageSize) + "," + strconv.Itoa(CurrentPage*PageSize)
+	return whereSql, nil
+}
+func GetWhereSqlLimt(TabName, ParameterStr string, PageSize, CurrentPage int) (string, error) {
+
+	whereSql, err := GetWhereSql(ParameterStr)
+	if err != nil {
+		return "", err
+	}
+
+	whereSql = "select * from  " + TabName + "  where " + whereSql + "  LIMIT " + strconv.Itoa((CurrentPage-1)*PageSize) + "," + strconv.Itoa(CurrentPage*PageSize)
+	return whereSql, nil
+}
+func GetWhereSqlCount(TabName, ParameterStr string) (string, error) {
+
+	whereSql, err := GetWhereSql(ParameterStr)
+	if err != nil {
+		return "", err
+	}
+
+	whereSql = "select count(1) as Num from  " + TabName + "  where " + whereSql
+	return whereSql, nil
+}
+func GetWhereSql(ParameterStr string) (string, error) {
+	sqlWhere := " 1=1 "
+	if ParameterStr == "" || ParameterStr == "[]" {
+		return sqlWhere, nil
+	}
+	var filterModelList []FilterModel
+
+	err := json.Unmarshal([]byte(ParameterStr), &filterModelList)
+	if err != nil {
+		return "", err
+	}
+	for i := 0; i < len(filterModelList); i++ {
+		fieldWhere, err := getFieldWhere(&(filterModelList[i]))
+		if err != nil {
+			return "", err
+		}
+		if sqlWhere == "" {
+			sqlWhere = "1=1 " + fieldWhere
+		} else {
+			sqlWhere += fieldWhere
+		}
+
+	}
+	return sqlWhere, nil
+}
+func getFieldWhere(model *FilterModel) (string, error) {
+	strTemp := ""
+	if model == nil {
+		return "", errors.New("对象为空")
+	}
+	if (*model).Logic == "" {
+		return "", errors.New("关系符为空")
+	}
+	if (*model).Action == "" {
+		return "", errors.New("算术符为空")
+	}
+	strTemp = " " + (*model).Logic
+	strTemp += " " + (*model).Column + " " + (*model).Action
+	strTemp += getwhereByDataType((*model).DataType)
+	strTemp += getwhereByAction((*model).Action)
+	strTemp += (*model).Value
+	strTemp += getwhereByAction((*model).Action)
+	strTemp += getwhereByDataType((*model).DataType)
+
+	return strTemp, nil
+
+}
+func getwhereByAction(action string) string {
+	action = strings.ToLower(action)
+	switch action {
+	case "like":
+		return "%"
+	default:
+		return ""
+	}
+}
+
+func getwhereByDataType(dataType string) string {
+	switch dataType {
+	case "S", "D":
+		return "'"
+	case "I", "F":
+		return ""
+	default:
+		return "'"
+	}
+}
+
+func DelSqlByField(TabName, FieldName string, Value interface{}) (string, error) {
+	var err error
+	var Sql string
+	defer func() {
+		if p := recover(); p != nil {
+			err = errors.New("类型断言错误")
+		}
+	}()
+	if Value == nil {
+		return "", errors.New("值为空")
+	}
+	Sql = " delete FROM " + TabName + " where  " + FieldName + " = "
+	Sql1 := ""
+	switch Value.(type) {
+	case int:
+		Sql1 = strconv.Itoa(Value.(int))
+	case int64:
+		Sql1 = strconv.FormatInt(Value.(int64), 10)
+	case float64:
+		Sql1 = strconv.FormatFloat(Value.(float64), 'E', -1, 64)
+	case string:
+		Sql1 = "'" + Value.(string) + "'"
+	default:
+		Sql1 = ""
+	}
+	Sql = Sql + Sql1
+	return Sql, err
+}
---
+++ whysql2.go
@@ -0,0 +1,262 @@
+package whysql
+
+import (
+	"encoding/json"
+	"errors"
+	"strconv"
+	"strings"
+)
+
+func NewWhy(ParameterStr string) (*WhyInfo, error) {
+	var err error
+
+	defer func() {
+		if p := recover(); p != nil {
+			err = errors.New("初始化异常")
+		}
+	}()
+	var filterModelList []FilterModel
+	m := new(WhyInfo)
+	if ParameterStr == "" || ParameterStr == "[]" {
+
+	} else {
+		err = json.Unmarshal([]byte(ParameterStr), &filterModelList)
+		if err != nil {
+			return nil, err
+		}
+	}
+	(*m).ColumnList = filterModelList
+	return m.Reset(), err
+}
+func (m *WhyInfo) Reset() *WhyInfo {
+	(*m).ParameterStr = ""
+	(*m).Limt = *new(LimtModel)
+	(*m).OrderByList = make([]OrderByModel, 0)
+	(*m).TabName = ""
+	(*m).CurrentPage = DefaultCurrentPage
+	(*m).PageSize = DefaultPageSize
+	(*m).IsLimt = false
+	return m
+}
+func (m *WhyInfo) Clear() *WhyInfo {
+	return m.Reset()
+}
+func (m *WhyInfo) SetTabName(tabName string) *WhyInfo {
+	(*m).TabName = tabName
+	return m
+}
+func (m *WhyInfo) SetPageSize(pageSize int) *WhyInfo {
+
+	if pageSize <= 0 {
+		pageSize = DefaultCurrentPage
+	}
+	(*m).PageSize = pageSize
+	(*m).IsLimt = true
+	return m
+}
+func (m *WhyInfo) SetCurrentPage(currentPage int) *WhyInfo {
+	if currentPage <= 0 {
+		currentPage = DefaultPageSize
+	}
+	(*m).CurrentPage = currentPage
+	(*m).IsLimt = true
+	return m
+}
+func (m *WhyInfo) SetLimt(args ...int) *WhyInfo {
+	if len(args) <= 0 {
+		(*m).CurrentPage = DefaultCurrentPage
+		(*m).PageSize = DefaultPageSize
+	} else if len(args) == 1 {
+		temp := args[0]
+		if temp <= 0 {
+			temp = DefaultCurrentPage
+		}
+		(*m).CurrentPage = temp
+		(*m).PageSize = DefaultPageSize
+	} else if len(args) > 1 {
+		temp := args[0]
+		if temp <= 0 {
+			temp = DefaultCurrentPage
+		}
+		temp1 := args[1]
+		if temp1 <= 0 {
+			temp1 = DefaultPageSize
+		}
+		(*m).CurrentPage = temp
+		(*m).PageSize = temp1 - temp + 1
+	}
+	(*m).IsLimt = true
+	return m
+}
+func (m *WhyInfo) SetOrderBy(rlist ...string) *WhyInfo {
+	tlist := make([]OrderByModel, 0)
+	if len(rlist) <= 0 {
+		return m
+	} else if len(rlist) == 1 {
+		tlist = append(tlist, OrderByModel{Column: rlist[0], SortType: DESC})
+	} else {
+		if strings.ToUpper(rlist[len(rlist)-1]) != DESC && strings.ToUpper(rlist[len(rlist)-1]) != ASC {
+			rlist = append(rlist, DESC)
+		}
+		for i := 0; i < len(rlist)-1; i++ {
+			tlist = append(tlist, OrderByModel{Column: rlist[i], SortType: rlist[len(rlist)-1]})
+		}
+	}
+
+	(*m).OrderByList = tlist
+	return m
+}
+func (m *WhyInfo) SetOrderByCustomize(rlist ...OrderByModel) *WhyInfo {
+	tlist := make([]OrderByModel, 0)
+	for _, val := range rlist {
+		tlist = append(tlist, val)
+	}
+	(*m).OrderByList = tlist
+	return m
+}
+func (m *WhyInfo) getLimt() {
+	PageSize := (*m).PageSize
+	CurrentPage := (*m).CurrentPage
+	min := (CurrentPage - 1) * PageSize
+	max := CurrentPage * PageSize
+	var temp LimtModel
+	temp.Min = min
+	temp.Max = max
+	(*m).Limt = temp
+
+}
+func (m *WhyInfo) getLimtStr() {
+	tempStr := ""
+	(*m).getLimt()
+	if (*m).Limt != (LimtModel{}) && (*m).IsLimt {
+		tempStr = " " + strconv.Itoa((*m).Limt.Min) + "," + strconv.Itoa((*m).Limt.Max) + " "
+	}
+	(*m).LimtStr = tempStr
+}
+func (m *WhyInfo) getOrderByStr() {
+	tempStr := ""
+	if (*m).OrderByList == nil || len((*m).OrderByList) <= 0 {
+
+	} else {
+		for i, val := range (*m).OrderByList {
+			if val != (OrderByModel{}) {
+				SortType := "Desc"
+				if val.SortType != "" {
+					SortType = val.SortType
+				}
+				tempStr = tempStr + " " + val.Column + " " + SortType + " "
+				if len((*m).OrderByList) != (i + 1) {
+					tempStr = tempStr + ", "
+				}
+			}
+
+		}
+
+	}
+	(*m).OrderByStr = tempStr
+}
+func (m *WhyInfo) getWhereSqlStr() error {
+	var err error
+	defer func() {
+		if p := recover(); p != nil {
+			err = errors.New("生成WHERER条件异常")
+		}
+	}()
+
+	tempStr := ""
+	for i := 0; i < len((*m).ColumnList); i++ {
+		fieldWhere, err := getFieldWhere(&((*m).ColumnList[i]))
+		if err != nil {
+			return err
+		}
+		if tempStr == "" {
+			tempStr = "1=1 " + fieldWhere
+		} else {
+			tempStr += fieldWhere
+		}
+
+	}
+	(*m).WhereStr = tempStr
+	return err
+}
+func (m *WhyInfo) getWhereInit() error {
+
+	tempStr := ""
+	(*m).Str = tempStr
+	var err error
+	defer func() {
+		if p := recover(); p != nil {
+			err = errors.New("生成WHERER条件异常")
+		}
+	}()
+
+	(*m).getOrderByStr()
+	(*m).getLimtStr()
+	err = (*m).getWhereSqlStr()
+	if err != nil {
+		return err
+	}
+	if (*m).WhereStr != "" {
+		tempStr = " where " + (*m).WhereStr
+	}
+
+	if (*m).OrderByStr != "" {
+		tempStr = tempStr + " Order By " + (*m).OrderByStr
+	}
+
+	if (*m).LimtStr != "" {
+		tempStr = tempStr + " LIMIT " + (*m).LimtStr
+	}
+	(*m).Str = tempStr
+	return err
+}
+func (m *WhyInfo) check() error {
+
+	if (*m).TabName == "" {
+		return errors.New("表名为空")
+	}
+	return nil
+
+}
+func (m *WhyInfo) GetQuerySql() (string, error) {
+	tempStr := ""
+	var err error
+	defer func() {
+		if p := recover(); p != nil {
+			err = errors.New("生成SQL异常")
+		}
+	}()
+	err = (*m).check()
+	if err != nil {
+		return "", err
+	}
+	err = (*m).getWhereInit()
+	if err != nil {
+		return "", err
+	}
+	tempStr = "select * from " + (*m).TabName + (*m).Str
+	(*m).Reset()
+	return tempStr, err
+
+}
+func (m *WhyInfo) GetCountSql() (string, error) {
+
+	tempStr := ""
+	var err error
+	defer func() {
+		if p := recover(); p != nil {
+			err = errors.New("生成SQL异常")
+		}
+	}()
+	err = (*m).check()
+	if err != nil {
+		return "", err
+	}
+	err = (*m).getWhereInit()
+	if err != nil {
+		return "", err
+	}
+	tempStr = "select count(1) as Num from " + (*m).TabName + (*m).Str
+	(*m).Reset()
+	return tempStr, err
+}
